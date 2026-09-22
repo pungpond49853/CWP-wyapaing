97249d92313cbedaeb998549f9d76eb94f8ee1d9
pungpond49853 <worapol.ya@gmail.com>
2026-09-23 02:53:45 +0700

    complete cell05 ex01

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for number in original_array:
+    new_array.append(number + 2)
+
+print("Original array:", original_array)
+print("New array:", new_array)
