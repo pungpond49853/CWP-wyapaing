c4f9552676f8814385fbeeeb3e5af7afdbe18170
pungpond49853 <worapol.ya@gmail.com>
2026-09-23 02:55:48 +0700

    complete cell05 ex02

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for number in original_array:
+    if number > 5:
+        new_array.append(number + 2)
+
+print(original_array)
+print(new_array)
