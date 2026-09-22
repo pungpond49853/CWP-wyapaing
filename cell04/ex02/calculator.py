e0af636dff3daa68d01f656cd2c0655e235bee42
pungpond49853 <worapol.ya@gmail.com>
2026-09-23 02:39:43 +0700

    complete cell04 ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+first_number = int(input("Give me the first number: "))
+second_number = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+print(f"{first_number} - {second_number} = {first_number - second_number}")
+print(f"{first_number} / {second_number} = {first_number / second_number:g}")
+print(f"{first_number} * {second_number} = {first_number * second_number}")
